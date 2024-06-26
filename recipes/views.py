e735d5a884f3c7b0f1429e80f86212835dceb920
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-26 13:15:54 +0000

    HTML linked to views.py and urls.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render (request, recipes/home.html)
+    return render(request, "recipes/home.html")
+    
+
+def about(request):
+    return render(request, "recipes/about.html")
+
+
 
     
