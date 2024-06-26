5237cdf54a32fc1f1342161fb75b453ef7ecd9c3
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-26 13:52:45 +0000

    basic test for recipe configuration

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, HttpResponse
 
+recipes = [
+    {
+        'author': 'Chris',
+        'title': 'Bangers & Mash',
+        'instructions': 'lorem ipsum',
+        'date_posted': 'June 24, 2024'
+    },
+    {
+        'author': 'Brendan',
+        'title': 'Chilli Con Carne',
+        'instructions': 'lorem ipsum dorem',
+        'date_posted': 'June 25, 2024'
+    },
+    {
+        'author': 'Simon',
+        'title': 'Chicken Tikka',
+        'instructions': 'lorem ipsum dorem title',
+        'date_posted': 'June 26, 2024'
+    } 
+]
+
 # Create your views here.
 def home(request):
-    return render(request, "recipes/home.html")
+    context = {
+        'recipes': recipes
+    }
+    return render(request, "recipes/home.html", context)
     
 
 def about(request):
