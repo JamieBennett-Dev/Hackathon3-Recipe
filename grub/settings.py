aa45699d0398b3ac27092ce5135dfbc44e64e632
Christopher Tranter <mr_crock@hotmail.co.uk>
2024-06-26 14:21:29 +0000

    my server connect to main

--- grub/settings.py
+++ grub/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-jamiebennet-hackathon3r-hb119c9mv7i.ws.codeinstitute-ide.net',
+    '8000-jamiebennet-hackathon3r-p1nhogmhsag.ws.codeinstitute-ide.net',
     '.herokuapp.com'
 ]
 
