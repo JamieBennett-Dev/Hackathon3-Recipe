80559e5caf5156e83160520c7174824382e03c52
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-27 14:14:54 +0000

    Iris fixed database

--- grub/settings.py
+++ grub/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env  # noqa
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,12 +90,18 @@ WSGI_APPLICATION = 'grub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-jamiebennet-hackathon3r-hb119c9mv7i.ws.codeinstitute-ide.net",
