7b1e6a14adf2635edca71dd944eb5d3623e7e32c
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-27 20:04:05 +0000

    changed allowed hosts and added user-login to register.html

--- grub/settings.py
+++ grub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8001-jamiebennet-hackathon3r-55a6xuz3ops.ws.codeinstitute-ide.net',
+    '8001-jamiebennet-hackathon3r-hb119c9mv7i.ws.codeinstitute-ide.net',
     '.herokuapp.com',
 ]
 
