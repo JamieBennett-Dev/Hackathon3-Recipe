8cf0d34b7a27ecfa7316714272a78fc88ad5d0a7
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-27 11:44:28 +0000

    deleted duplicate settings

--- grub/settings.py
+++ grub/settings.py
@@ -281,12 +281,3 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
