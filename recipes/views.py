8ca566b136ed5774a62d250466345245752ad08e
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-27 19:29:28 +0000

    CRUD p1, add recipe_detail.html and recipe_form.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
 
 def home(request):
@@ -7,6 +9,22 @@ def home(request):
     'recipes': recipes
   }
   return render(request, 'recipes/home.html', context)
+
+class RecipeListView(ListView):
+  model = models.Recipe
+  template_name = 'recipes/home.html'
+  context_object_name = 'recipes'
+
+class RecipeDetailView(DetailView):
+  model = models.Recipe
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+  model = models.Recipe
+  fields = ['title', 'description']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
 
 def about(request):
   return render(request, 'recipes/about.html', {'title': 'about page'})
